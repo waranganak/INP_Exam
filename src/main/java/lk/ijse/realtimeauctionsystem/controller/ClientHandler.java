c94754503dae2815032cf661d2bbeb1e8c85a27b
kauminiwarangana <kauminiwarangana143@gmail.com>
2026-06-09 11:11:08 +0530

    Add Client1.fxml Add ClientHandler.java

--- src/main/java/lk/ijse/realtimeauctionsystem/controller/ClientHandler.java
+++ src/main/java/lk/ijse/realtimeauctionsystem/controller/ClientHandler.java
@@ -1,9 +1,34 @@
 package lk.ijse.realtimeauctionsystem.controller;
 
-public class ClientHandler {
+import lk.ijse.realtimeauctionsystem.AuctionServer;
+import java.io.*;
+import java.net.Socket;
 
+public class ClientHandler implements Runnable {
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private String username;
 
+    public ClientHandler(Socket socket) throws IOException {
+        this.socket = socket;
+        this.out = new PrintWriter(socket.getOutputStream(), true);
+        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
 
+    public void send(String msg) { out.println(msg); }
 
+    @Override
+    public void run() {
+        try {
+            username = in.readLine();
+            System.out.println("Client connected: " + username);
+            String line;
+            while ((line = in.readLine()) != null) {
+                AuctionServer.processBid(username, Double.parseDouble(line), this);
+            }
+        } catch (Exception e) {
+            System.out.println("Client disconnected: " + username);
+        }
+    }
 }
-
